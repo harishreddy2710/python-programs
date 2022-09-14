aa1fec6ebfab9bd27aa3c67f22ebf48f8268a791
munugalahari <harishreddy2710@gmail.com>
2022-09-14 13:07:52 +0530

    adding  files

--- sam1.py
+++ sam1.py
@@ -385,3 +385,143 @@
 # print_dict()
 
 # 35.
+# def printDict():
+#     d=dict()
+#     for i in range(1,21):
+#         d[i]=i**2
+#     for k in d.keys():
+#         print(k)
+#     for j in d.values():
+#         print(j)
+#
+# printDict()
+
+# 36.
+# def printList():
+#     li = list()
+#     for i in range(1, 21):
+#         li.append(i ** 2)
+#     print(li)
+#
+#
+# printList()
+
+# 37.Define a function which can generate a list where the values are
+# square of numbers between 1 and 20 (both included). Then the function
+# needs to print the first 5 elements in the list.
+# Hints:
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use list.append() to add values into a list.
+# Use [n1:n2] to slice a list
+# Solution:
+
+# def printList():
+#     li=list()
+#     for i in range(1,21):
+#         li.append(i**2)
+#     print(li[:20])
+# printList()
+
+# 38.
+#
+# Define a function which can generate a list where the values are
+# square of numbers between 1 and 20 (both included). Then the function
+# needs to print the last 5 elements in the list.
+# Hints:
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use list.append() to add values into a list.
+# Use [n1:n2] to slice a list
+# Solution
+# def printList():
+#     li=list()
+#     for i in range(1,21):
+#         li.append(i**2)
+#     print(li[-5:])
+# printList()
+
+# 39.
+# Define a function which can generate a list where the values are
+# square of numbers between 1 and 20 (both included). Then the function
+# needs to print all values except the first 5 elements in the list.
+# Hints:
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use list.append() to add values into a list.
+# Use [n1:n2] to slice a list
+# Solution
+# def printList():
+#     li=list()
+#     for i in range(1,21):
+#         li.append(i**2)
+#     print(li[5:])
+# printList()
+
+# 40.
+# Define a function which can generate and print a tuple where the value
+# are square of numbers between 1 and 20 (both included).
+# Hints:
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use list.append() to add values into a list.
+# Use tuple() to get a tuple from a list.
+# # Solution
+# def printTuple():
+#     li=list()
+#     for i in range(1,21):
+#         li.append(i**2)
+#     print(tuple(li))
+# printTuple()
+
+# 41.
+# With a given tuple (1,2,3,4,5,6,7,8,9,10), write a program to print
+# the first half values in one line and the last half values in one
+# line.
+# Hints:
+# Use [n1:n2] notation to get a slice from a tuple.
+# Solution
+# tp=(1,2,3,4,5,6,7,8,9,10)
+# tp1=tp[:5]
+# tp2=tp[5:]
+# print(tp1)
+# print(tp2)
+
+# 42.
+# Write a program to generate and print another tuple whose values are
+# even numbers in the given tuple (1,2,3,4,5,6,7,8,9,10).
+# Hints:
+# Use "for" to iterate the tuple
+# Use tuple() to generate a tuple from a list.
+# Solution
+# tp = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# li = list()
+# for i in tp:
+#     if i % 2 == 0:
+#         li.append(i)
+# tp2 = tuple(li)
+# print(tp2)
+
+# 43.
+# Write a program which accepts a string as input to print "Yes" if the
+# string is "yes" or "YES" or "Yes", otherwise print "No".
+# Hints:
+# Use if statement to judge condition.
+# Solution
+# s = input()
+# if s == "yes" or s == "YES" or s == "Yes":
+#     print("Yes")
+# else:
+#     print("No")
+
+# 44.
+
+# Write a program which can filter even numbers in a list by using
+# filter function. The list is: [1,2,3,4,5,6,7,8,9,10].
+# Hints:
+# Use filter() to filter some elements in a list.
+# Use lambda to define anonymous functions.
+# Solution
+li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+evenNumbers = filter(lambda x: x % 2 == 0, li)
+print(evenNumbers)
